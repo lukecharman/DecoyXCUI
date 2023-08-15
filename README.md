960aa67192fbb6c9a82357f469482d226530256c
Luke Charman <hello@lukecharman.com>
2023-08-15 11:19:07 +0100

    Comments, and cleanup.

--- README.md
+++ README.md
@@ -1,21 +1,40 @@
 # DecoyXCUI
 
-An `XCTestCase` subclass named `DecoyUITestCase` from which all Decoy tests should inherit.
+`DecoyXCUI` is an extension to the `Decoy` framework, providing an `XCTestCase` subclass named `DecoyUITestCase`.
 
-This package should be imported into your UI testing target, not your app target. To do this:
+This subclass is intended to be the foundation from which all Decoy tests should inherit.
+
+*Note: This package should be imported into your UI testing target, not your app target.*
+
+## Setup Instructions
+
 * Tap your app's project in the Project Navigator.
 * Under "Targets", tap your app's UI testing target.
 * Tap Build Phases.
 * Unfold the "Link Binary With Libraries" section.
-* Use the plus icon to add both `Decoy` and `DecoyXCUI` to the list.
+* Use the plus icon to add both Decoy and DecoyXCUI to the list.
 * Ensure they are both assigned the 'Required' status.
 
-In your own test classes that subclass `DecoyUITestCase`, there's just one thing to do:
-* Call the custom `setUp()` method, like so, passing in whether or not you'd like to record.
-* This will launch your app with the requires environment variables to use Decoy.
+## Usage
+
+In your test classes that subclass `DecoyUITestCase`, initiate the setup as follows:
 
 ```
 override func setUp() {
-  super.setUp(recording: false)
+  super.setUpDecoy()
 }
 ```
+
+When you want to record a decoy, set up like this:
+
+```
+override func setUp() {
+  super.setUpDecoy(isRecording: true)
+}
+```
+
+This will launch your app with the required environment variables to use Decoy.
+
+## Feedback & Contributions
+
+Feel free to open an issue or submit a pull request if you have any improvements or feedback.
--- Sources/DecoyXCUI/DecoyUITestCase.swift
+++ Sources/DecoyXCUI/DecoyUITestCase.swift
@@ -1,39 +1,86 @@
 import XCTest
 import Decoy
 
+/// Custom test case class tailored for Decoy-based UI tests.
 open class DecoyUITestCase: XCTestCase {
 
+  /// Application instance used for UI tests.
   public var app: XCUIApplication!
 
-  public func setUp(path: String = #filePath, recording: Bool) {
+  /// Sets up the testing environment for Decoy-based UI tests.
+  /// - Parameters:
+  ///   - path: The file path at which decoy configurations will be read from or written to. Defaults to the current file path
+  ///   - isRecording: Indicates whether the test is in recording mode. Defaults to `false`.
+  public func setUpDecoy(path: String = #filePath, isRecording: Bool = false) {
     super.setUp()
 
-    var url = URL(string: path)!.deletingLastPathComponent()
-    url.safeAppend(path: DecoyHub.Constants.decoysFolder)
+    /// Prepare the URL based on the provided path.
+    guard var url = preparedURL(path: path) else {
+      XCTFail("Setup failed to resolve path: \(path) to a URL.")
+      return
+    }
 
+    /// Extract the decoy name based on the test's name.
+    guard let decoyName = decoyName else {
+      XCTFail("Could not resolve test name from \(name)")
+      return
+    }
+
+    /// Initialize the app instance.
     app = XCUIApplication()
 
-    app.launchEnvironment[DecoyHub.Constants.isRecording] = String(recording)
-    app.launchEnvironment[DecoyHub.Constants.isXCUI] = String(true)
-    app.launchEnvironment[DecoyHub.Constants.decoyPath] = url.absoluteString
-    app.launchEnvironment[DecoyHub.Constants.decoyFilename] = "\(decoyName).json"
+    /// Set environment variables for the app launch.
+    prepareEnvironment(url: url, isRecording: isRecording, name: name)
 
+    /// Launch the application for UI testing.
     app.launch()
   }
-}
 
-public extension DecoyUITestCase {
-
-  var decoyName: String {
-    name
-      .split(separator: " ")
-      .last!
+  /// Extracts the decoy name from the test's name by removing any trailing "]" and retrieving the last component.
+  public var decoyName: String? {
+    guard let decoyName = name
       .replacingOccurrences(of: "]", with: "")
+      .split(separator: " ")
+      .last
+    else {
+      return nil
+    }
+
+    return String(decoyName)
   }
 }
 
+private extension DecoyUITestCase {
+  /// Modifies the provided path to derive the URL for decoy configurations.
+  /// - Parameter path: The original file path.
+  /// - Returns: A modified URL pointing to the decoy configurations or `nil` if the URL can't be constructed.
+  func preparedURL(path: String) -> URL? {
+    var url = URL(string: path)
+
+    url?.deleteLastPathComponent()
+    url?.safeAppend(path: DecoyHub.Constants.decoysFolder)
+
+    return url
+  }
+
+  /// Sets the necessary environment variables before the app launch.
+  /// - Parameters:
+  ///   - url: The URL pointing to decoy configurations.
+  ///   - isRecording: Indicates whether the test is in recording mode.
+  ///   - name: The name of the test.
+  func prepareEnvironment(url: URL, isRecording: Bool, name: String) {
+    app.launchEnvironment[DecoyHub.Constants.isRecording] = String(isRecording)
+    app.launchEnvironment[DecoyHub.Constants.isXCUI] = String(true)
+    app.launchEnvironment[DecoyHub.Constants.decoyPath] = url.absoluteString
+    app.launchEnvironment[DecoyHub.Constants.decoyFilename] = (name + ".json")
+  }
+}
+
+/// Extension for the URL class to provide safer path operations tailored for different iOS versions.
 private extension URL {
 
+  /// Initializes a URL based on the file path, considering iOS version specifics.
+  /// - Parameter path: The file path.
   init(safePath path: String) {
     if #available(iOS 16, *) {
       self.init(filePath: path)
@@ -42,6 +89,8 @@ private extension URL {
     }
   }
 
+  /// Appends a path component to the URL, considering iOS version specifics.
+  /// - Parameter path: The path component to append.
   mutating func safeAppend(path: String) {
     if #available(iOS 16, *) {
       self.append(path: path)
